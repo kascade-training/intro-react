74ef436716a9f3d0e1090fd9b9b406b15ee4a0b5
Ahmed <ahhosni@vistaprint.com>
2020-06-04 01:23:27 +0100

    change props for students rendering

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,6 @@ class App extends React.Component {
         />
       );
     }
-
     /* Change below to render students*/
 
     if (this.state.buttonClicked === "students") {
@@ -81,7 +80,7 @@ class App extends React.Component {
     }
 
     /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -90,7 +89,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
